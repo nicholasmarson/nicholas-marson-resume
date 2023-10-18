22939c6a1ac8653ae5643315edc30a52ad5aded9
nicholasmarson <nicholasmarson@Nicholass-MBP.attlocal.net>
2023-10-18 11:12:04 -0400

    Added About Me Message. Starting pop-out menu now

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,18 +1,41 @@
 <template>
+<div id="about-view">
   <header-comp />
+  <about-me />
   <footer-comp />
+  </div>
 </template>
 
 <script>
 import HeaderComp from "@/components/HeaderComp.vue";
 import FooterComp from "@/components/FooterComp.vue";
+import AboutMe from '@/components/AboutMe.vue';
 export default {
   components: {
     HeaderComp,
     FooterComp,
+    AboutMe,
   },
 };
 </script>
 
 <style>
+#about-view {
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+}
+
+#header-comp {
+  height: 30%;
+}
+
+#about-me {
+  height: 60%;
+}
+
+#footer-container {
+  height: 10%;
+}
 </style>
